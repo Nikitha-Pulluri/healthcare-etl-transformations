b56f9c77e91f13c8117535f828dbdff68f39ac39
Nikitha Pulluri <npulluri26@gmail.com>
2025-06-18 14:42:53 -0400

    Delete retention_flag.py

--- retention_flag.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime, timedelta
-
-threshold_date = datetime.now() - timedelta(days=730)
-df['retention_flag'] = df['start_date'].apply(lambda x: 'Review' if x < threshold_date else 'Retain')
