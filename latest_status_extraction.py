94165658dc66e111b6f866b52ef38d35a4280842
Nikitha Pulluri <npulluri26@gmail.com>
2025-06-18 14:42:35 -0400

    Delete latest_status_extraction.py

--- latest_status_extraction.py
+++
@@ -1,6 +0,0 @@
-def get_latest_status(log):
-    if log:
-        return sorted(log, key=lambda x: x['date'], reverse=True)[0]['status']
-    return 'unknown'
-
-df['latest_status'] = df['status_log'].apply(get_latest_status)
